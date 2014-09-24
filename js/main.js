bad81e34ce621278b6f8f07d79d38b6d848fc5a9
michaelevensen <michaelevensen@Michaels-MacBook-Pro.local>
2014-09-24 15:34:40 +0200

    Final Version before Djangofying

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 
+$(function() {
+
+	// Detect Browser Height
+	var window_height = $(window).height();
+
+	// Only set height for bigger screens
+	if (window_height>1000) {
+
+		$('.hero').height(window_height - 50);
+	}
+});
