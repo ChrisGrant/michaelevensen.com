ef4d7dc829da5ac83a73e7913b24b62491312a9b
michaelevensen <michaelevensen@Michaels-MacBook-Pro.local>
2014-09-26 20:33:11 +0200

    Revert bad81e3..ee8cb83
    This rolls back to commit bad81e34ce621278b6f8f07d79d38b6d848fc5a9.

--- michaelevensen/michaelevensen/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/michaelevensen/Sites/michaelevensen.com/michaelevensen/michaelevensen/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
